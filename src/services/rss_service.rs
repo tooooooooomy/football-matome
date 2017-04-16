2a21ccd9d95a504bf5c9468028ffc1f3b95139b1
Tomy <kazushige.tominaga@mkt-apps.com>
2017-04-16 13:38:48 +0000

    fix to use map

--- src/services/rss_service.rs
+++ src/services/rss_service.rs
@@ -1,9 +1,11 @@
+extern crate chrono;
+
 use diesel::prelude::*;
 use diesel::mysql::MysqlConnection;
 use models::feed::Feed;
 use schema::feeds::dsl::*;
 
-#[derive(RustcEncodable)]
+#[derive(RustcEncodable, Debug, PartialEq)]
 pub struct ResFeed {
     id: i32,
     title: String,
@@ -20,10 +22,31 @@ pub fn retrieve(conn: &MysqlConnection) -> Vec<ResFeed>{
         .collect::<Vec<ResFeed>>()
 }
 
-fn make_res_feed_from_feed (feed: Feed) -> ResFeed {
+fn make_res_feed_from_feed(feed: Feed) -> ResFeed {
     ResFeed {
         id: feed.id,
         title: feed.title,
         link: feed.link,
     }
+}
+
+#[test]
+fn test_make_res_feed_from_feed() {
+    use chrono::prelude::*;
+
+    let expected = ResFeed {
+        id: 1,
+        title: "hoge".to_string(),
+        link: "http://hoge.com".to_string(),
+    };
+
+    let result = make_res_feed_from_feed(Feed {
+        id: 1,
+        title: "hoge".to_string(),
+        link: "http://hoge.com".to_string(),
+        created_at: UTC::now().naive_utc(),
+        updated_at: UTC::now().naive_utc(),
+    });
+
+    assert_eq!(expected, result);
 }
