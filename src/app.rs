85fa875f11577f526e34fde8b21b5ac648b5f8a1
Tomy <kazushige.tominaga@mkt-apps.com>
2017-05-01 00:00:34 +0000

    up

--- src/app.rs
+++ src/app.rs
@@ -9,3 +9,56 @@ pub fn start_server(address: &str) -> Result<ListeningServer, Box<StdError>>{
 
     server.listen(address)
 }
+
+#[cfg(test)]
+mod tests {
+    use self::support::{Server, STATIC_SERVER, get};
+    use hyper::header;
+    use nickel::status::StatusCode;
+    use rustc_serialize::json::Json;
+
+    #[test]
+    fn get_empty_feeds() {
+        let mut response = get("/football-matome/api/get");
+
+        assert_eq!(StatusCode::Ok, response.status);
+    }
+
+    mod support {
+        use hyper::client::{Client, Response as HyperResponse};
+        use nickel::ListeningServer;
+        use std::net::SocketAddr;
+
+        pub struct Server(SocketAddr);
+        impl Server {
+            pub fn new(server: ListeningServer) -> Server{
+                let wrapped = Server(server.socket());
+
+                server.detach();
+
+                wrapped
+            }
+
+            pub fn get(&self, path: &str) -> HyperResponse {
+                let url = self.url_for(path);
+                Client::new().get(&url).send().unwrap()
+            }
+
+            pub fn url_for(&self, path: &str) -> String{
+                format!("http://{}{}", self.0, path)
+            }
+        }
+
+        // This is a shared instance of the server between all the tests
+        lazy_static! {
+            pub static ref STATIC_SERVER: Server = {
+                let server = super::super::start_server("127.0.0.1:0").unwrap();
+                Server::new(server)
+            };
+        }
+
+        pub fn get(path: &str) -> HyperResponse {
+            STATIC_SERVER.get(path)
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 #[macro_use] extern crate diesel;
 #[macro_use] extern crate diesel_codegen;
-extern crate nickel;
-extern crate dotenv;
-extern crate rustc_serialize;
+#[macro_use] extern crate lazy_static;
 extern crate chrono;
+extern crate dotenv;
+extern crate hyper;
+extern crate nickel;
+extern crate rustc_serialize;
 
 pub mod app;
-pub mod schema;
+pub mod controllers;
 pub mod models;
 pub mod libraries;
+pub mod schema;
 pub mod services;
-pub mod controllers;
