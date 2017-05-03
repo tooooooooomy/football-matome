4e14241b5efec8dc9847ad4d7e39e634448bfd7b
Tomy <kazushige.tominaga@mkt-apps.com>
2017-05-03 02:17:03 +0000

    add integration test

--- src/app.rs
+++ src/app.rs
@@ -12,22 +12,41 @@ pub fn start_server(address: &str) -> Result<ListeningServer, Box<StdError>>{
 
 #[cfg(test)]
 mod tests {
-    use self::support::{Server, STATIC_SERVER, get};
+    use self::support::{Body, get};
     use hyper::header;
-    use nickel::status::StatusCode;
+    use hyper::status::StatusCode;
     use rustc_serialize::json::Json;
+    use std::env;
 
     #[test]
     fn get_empty_feeds() {
+        env::set_var("DATABASE_URL", "mysql://root@localhost/test_football_matome");
         let mut response = get("/football-matome/api/get");
+        let json = Json::from_str(&response.body()).unwrap();
 
         assert_eq!(StatusCode::Ok, response.status);
+        assert_eq!(response.headers.get::<header::ContentType>(), Some(&header::ContentType::json()));
+        assert_eq!(Some(&vec![]), json["data"].as_array());
     }
 
     mod support {
         use hyper::client::{Client, Response as HyperResponse};
         use nickel::ListeningServer;
         use std::net::SocketAddr;
+
+        pub trait Body {
+            fn body(self) -> String;
+        }
+
+        impl<'a> Body for &'a mut HyperResponse {
+            fn body(self) -> String {
+                use std::io::Read;
+                let mut body = String::new();
+                self.read_to_string(&mut body).expect("Failed to read body from response");
+
+                body
+            }
+        }
 
         pub struct Server(SocketAddr);
         impl Server {
