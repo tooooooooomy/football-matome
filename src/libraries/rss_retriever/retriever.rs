9cc9e2259f32be75be3731d7ab473710b7abcdfd
Tomy <kazushige.tominaga@mkt-apps.com>
2017-05-27 01:55:22 +0000

    up

--- src/libraries/rss_retriever/retriever.rs
+++ src/libraries/rss_retriever/retriever.rs
@@ -95,6 +95,10 @@ impl Retriever {
 mod tests {
 
     use mockito;
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::env;
+    use std::path::Path;
 
     const URL: &'static str = mockito::SERVER_URL;
 
@@ -110,5 +114,50 @@ mod tests {
         let t = super::Retriever::new(URL);
 
         t.get_item_list();
+    }
+
+    #[test]
+    fn test_get_item_list() {
+        let current_dir = env::current_dir().unwrap();
+        let file_path = format!("{}/src/libraries/rss_retriever/stab/matome.rdf", current_dir.display());
+        let mut file = File::open(file_path).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
+        mockito::mock("GET", "/")
+            .with_status(200)
+            .with_header("content-type", "text/xml; charset=utf-8")
+            .with_body(&contents)
+            .create();
+
+        let t = super::Retriever::new(URL);
+
+        let result = t.get_item_list();
+
+        let expected = (vec![
+            "【ネイマール】30日に緊急来日決定！スポンサー企業の催しに出席しサイン会も実施。".to_string(),
+            "【日本代表】選考基準は“ケンカ上等”？「ピッチ状態も今回はキーになる」".to_string(),
+            "【ハリル監督】エース交代！就任以来初めて本田を久保に次ぐ2番手として評価！".to_string(),
+            "【オーバメヤン】ドルトムント退団を希望と独紙報道！100億円で移籍を容認へ。".to_string(),
+            "【日本代表】なぜ本田圭佑は生き残ったのか？常連組が消えたハリルジャパン".to_string(),
+            "【画像】今季JリーグGKセーブ率ランキング…1人神がいた！？wwww".to_string(),
+            "マンUの堅守速攻が許せない？アヤックス指揮官&クラーセン主将が不満！「引いて、ロングボール蹴るのみ」".to_string(),
+            "【画像】日本代表、シリア戦は“炎モデル”…W杯初出場決定20周年メモリアルユニを着用".to_string(),
+            "【日本代表】“川口の再来”中村航輔が代表入り！最近の神がかりっぷりがやばい".to_string(),
+            "【ネタ動画】CLに出れないアーセナル、ぼろくそでワロタwwwwwwwww".to_string()
+        ], vec![
+            "http://samuraigoal.doorblog.jp/archives/51364634.html".to_string(),
+            "http://samuraigoal.doorblog.jp/archives/51364269.html".to_string(),
+            "http://samuraigoal.doorblog.jp/archives/51363949.html".to_string(),
+            "http://samuraigoal.doorblog.jp/archives/51363357.html".to_string(),
+            "http://samuraigoal.doorblog.jp/archives/51361565.html".to_string(),
+            "http://samuraigoal.doorblog.jp/archives/51361065.html".to_string(),
+            "http://samuraigoal.doorblog.jp/archives/51361288.html".to_string(),
+            "http://samuraigoal.doorblog.jp/archives/51361361.html".to_string(),
+            "http://samuraigoal.doorblog.jp/archives/51361175.html".to_string(),
+            "http://samuraigoal.doorblog.jp/archives/51354844.html".to_string()
+        ]);
+
+        assert_eq!(expected, result);
     }
 }
